aec6deca8260b22376b002ee2697a6faec90b528
Karen Shaw <karendid@gmail.com>
2025-02-06 21:14:46 +0000

    Hard code staging stack prefix in pipeline

--- osdp/pipeline/osdp_application_stage.py
+++ osdp/pipeline/osdp_application_stage.py
@@ -9,5 +9,5 @@ class OsdpApplicationStage(Stage):
         super().__init__(scope, id, **kwargs)
         OsdpPrototypeStack(
             self,
-            f"{self.node.try_get_context('stack_prefix')}-OSDP-Prototype"
+            "staging-OSDP-Prototype"
         )
