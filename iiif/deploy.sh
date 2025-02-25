2d9a24441541f8ccd6cdea0cc7ddd9923f3d16b7
charlesLoder <charles.loder@northwestern.edu>
2025-02-25 20:37:11 +0000

    Update deploy script to use ecr public

--- iiif/deploy.sh
+++ iiif/deploy.sh
@@ -9,16 +9,16 @@ IMAGE_TAG="latest"
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
 # Login to ECR
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+aws ecr-public get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin public.ecr.aws
 
 # Create ECR repository if it doesn't exist
-aws ecr create-repository --repository-name $ECR_REPOSITORY_NAME --region $AWS_REGION || true
+aws ecr-public create-repository --repository-name $ECR_REPOSITORY_NAME --region $AWS_REGION || true
 
 # Build Docker image
 docker build -t $ECR_REPOSITORY_NAME:$IMAGE_TAG .
 
 # Tag image for ECR
-docker tag $ECR_REPOSITORY_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_NAME:$IMAGE_TAG
+docker tag $ECR_REPOSITORY_NAME:$IMAGE_TAG public.ecr.aws/$ECR_REPOSITORY_NAME:$IMAGE_TAG
 
 # Push image to ECR
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_NAME:$IMAGE_TAG
+docker push public.ecr.aws/$ECR_REPOSITORY_NAME:$IMAGE_TAG
