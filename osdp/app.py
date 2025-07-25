59ce68a8b2526aca2a38da822221a10a85f6171e
Karen Shaw <karendid@gmail.com>
2025-07-25 15:40:11 +0000

    Fix typo in repository

--- osdp/app.py
+++ osdp/app.py
@@ -40,7 +40,7 @@ else:
     # Manually construct the 'data' dictionary from flat CLI context if needed
     data_context = app.node.try_get_context("data")
     ecr_context = app.node.try_get_context("ecr")
-    if (data_context is None or not isinstance(data_context, dict)):
+    if data_context is None or not isinstance(data_context, dict):
         print("Constructing 'data' context from individual CLI parameters...")
         data_type_cli = app.node.try_get_context("data.type")
         if data_type_cli:
@@ -53,12 +53,15 @@ else:
                     "prefix": app.node.try_get_context("data.s3.prefix"),
                 }
             app.node.set_context("data", constructed_data)
-            if (ecr_context is None):
-                app.node.set_context("ecr", {
-                    "registry": app.node.try_get_context("ecr.registry"),
-                    "respository": app.node.try_get_context("ecr.repository"),
-                    "tag": app.node.try_get_context("ecr.tag")
-                })
+            if ecr_context is None:
+                app.node.set_context(
+                    "ecr",
+                    {
+                        "registry": app.node.try_get_context("ecr.registry"),
+                        "repository": app.node.try_get_context("ecr.repository"),
+                        "tag": app.node.try_get_context("ecr.tag"),
+                    },
+                )
             else:
                 print("Warning: 'ecr' configuration required for IIIF data type.")
         else:
